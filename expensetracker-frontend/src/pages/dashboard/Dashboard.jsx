cf0b9330957f641c9cdaecaa37d0bb1f3fa54b42
Emil Ismayilzada <ismailzadeh.emil@gmail.com>
2022-01-27 12:19:18 +0100

    created receiptHistory

--- expensetracker-frontend/src/pages/dashboard/Dashboard.jsx
+++ expensetracker-frontend/src/pages/dashboard/Dashboard.jsx
@@ -7,8 +7,8 @@ import ReceiptHistory from "./ReceiptHistory";
 export default function Dashboard() {
     return (
         <VStack px='4'>
-            <Center width={'full'} h='400px' border={'1px'}>
-                <ReceiptHistory />
+            <Center width={'full'} h='400px'>
+                <ReceiptHistory/>
             </Center>
         </VStack>
     );
--- expensetracker-frontend/src/pages/dashboard/ReceiptHistory.jsx
+++ expensetracker-frontend/src/pages/dashboard/ReceiptHistory.jsx
@@ -5,27 +5,105 @@ import {
     Link,
     VStack,
     Grid,
+    Table,
+    Thead,
+    Tbody,
+    Tag,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    chakra
   } from '@chakra-ui/react';
-export default function ReceiptHistory(){
-    return (
-        <Box textAlign="center" fontSize="xl" border='1px' borderColor='gray.500' borderRadius='10px'>
-        
-        <Grid p={3}>
-          <VStack spacing={8}>
-            <Text>
-              Dashboard
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    );
+import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons'
+import { useTable, useSortBy } from 'react-table'
+export default function ReceiptHistory() {
+  const data = React.useMemo(
+    () => [
+      {
+        merchantName: 'inches',
+        categoryName: 'millimetres (mm)',
+        spentAmount: 25.4,
+      },
+      {
+        merchantName: 'feet',
+        categoryName: 'centimetres (cm)',
+        spentAmount: 30.48,
+      },
+      {
+        merchantName: 'yards',
+        categoryName: 'metres (m)',
+        spentAmount: 0.91444,
+      },
+    ],
+    [],
+  )
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Merchant',
+        accessor: 'merchantName',
+      },
+      {
+        Header: 'Category',
+        accessor: 'categoryName',
+      },
+      {
+        Header: 'Spent',
+        accessor: 'spentAmount',
+        isNumeric: true,
+      },
+    ],
+    [],
+  )
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data }, useSortBy)
+
+  return (
+    <Box fontSize="l" border='1px' borderColor='gray.100' borderRadius='10px' width={'full'}>
+
+    <Table {...getTableProps()} colorScheme={'gray'} boxShadow={'lg'}>
+  <TableCaption placement={'top'} fontSize={'2xl'} textAlign={'left'}>Receipt History</TableCaption>
+      <Thead>
+        {headerGroups.map((headerGroup) => (
+          <Tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <Th
+                {...column.getHeaderProps(column.getSortByToggleProps())}
+                isNumeric={column.isNumeric}
+              >
+                {column.render('Header')}
+                <chakra.span pl='4'>
+                  {column.isSorted ? (
+                    column.isSortedDesc ? (
+                      <TriangleDownIcon aria-label='sorted descending' />
+                    ) : (
+                      <TriangleUpIcon aria-label='sorted ascending' />
+                    )
+                  ) : null}
+                </chakra.span>
+              </Th>
+            ))}
+          </Tr>
+        ))}
+      </Thead>
+      <Tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row)
+          return (
+            <Tr {...row.getRowProps()}>
+              {row.cells.map((cell) => (
+                <Td {...cell.getCellProps()} isNumeric={cell.column.isNumeric}>
+                  {cell.render('Cell')}
+                </Td>
+              ))}
+            </Tr>
+          )
+        })}
+      </Tbody>
+    </Table>
+    </Box>
+  )
 }
