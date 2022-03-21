0cd0f9567dbe9d1e70cc93c3fce39c0ae5b959a6
Emil Ismayilzada <iamemil@users.noreply.github.com>
2022-03-21 23:55:23 +0100

    fixed server issue

--- expensetracker-frontend/src/api/constants.js
+++ expensetracker-frontend/src/api/constants.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://localhost:44396";
-//export const API_URL = "http://ismayilzada-001-site1.ctempurl.com";
+//export const API_URL = "https://localhost:44396";
+export const API_URL = "https://receipttracker.me";
