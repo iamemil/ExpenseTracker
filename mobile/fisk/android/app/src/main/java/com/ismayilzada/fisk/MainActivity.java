3dc0a62899b62168df63c70042ba8c1034a40764
Emil Ismayilzada <ismailzadeh.emil@gmail.com>
2022-07-04 22:12:47 +0200

    added more flutter files

---
+++ mobile/fisk/android/app/src/main/java/com/ismayilzada/fisk/MainActivity.java
@@ -0,0 +1,6 @@
+package com.ismayilzada.fisk;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ mobile/fisk/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:fisk/presentation/pages/login/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fisk',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
